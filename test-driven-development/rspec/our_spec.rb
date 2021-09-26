4b214206d68ccbc911648979e94c83cc0eaa4183
Delia <delia.bute@yahoo.com>
2021-09-26 21:16:45 +0100

    building my own rspec

---
+++ test-driven-development/rspec/our_spec.rb
@@ -0,0 +1,62 @@
+require "colorize"
+
+class OurSpec
+  def self.describe(name, &block)
+    puts name
+    instance_eval &block
+  end
+
+  def self.context(name, &block)
+    puts "  #{name}"
+    instance_eval &block
+  end
+
+  def self.it (name,&block)
+    puts "      #{name}"
+    instance_eval &block
+  end
+
+  def self.expect(value)
+    Expector.new(value)
+  end
+  
+  def self.eq (value)
+    value
+  end
+
+
+  class Expector
+    attr_accessor :expected_value
+
+    def initialize(expected_value)
+      @expected_value = expected_value
+    end
+
+    def to(value)
+      passed = @expected_value == value
+
+      if passed
+        puts "      it passed!".green
+      else
+        puts "      it failed!".red
+      end
+    end
+  end
+
+end
+
+OurSpec.describe 'Dog' do
+  context "when it is hungry" do
+    it "begs for food" do
+      expect(true).to eq true
+    end
+
+    it "salivates" do
+      expect(true).to eq true
+    end
+
+    it "does not sit quietly" do
+      expect(false).to eq true
+    end
+  end
+end
---
+++ test-driven-development/rspec/spec/dog_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe "Dog" do
+  context "when it is hungry" do
+    it "begs for food" do
+      expect(true).to eq true
+    end
+  end
+end
