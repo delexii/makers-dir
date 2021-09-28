907d323989993dac74c6991dcc97455a1d994ce2
Delia <delia.bute@yahoo.com>
2021-09-28 15:07:25 +0100

    added touch in and out features and raised error when insufficient minimum balance

--- object-oriented-design/oystercard/lib/oystercard.rb
+++ object-oriented-design/oystercard/lib/oystercard.rb
@@ -1,18 +1,33 @@
 class Oystercard
-  attr_accessor :balance
+  attr_accessor :balance, :in_use
 
   LIMIT = 90
+  MIN_AMOUNT = 1
 
   def initialize
     @balance = 0
+    @in_use = false
   end
 
   def top_up(num)
     raise "Card limit exceeded (£#{LIMIT})" if num + self.balance > LIMIT
     @balance += num
   end
-  
+
   def deduct(num)
     @balance -= num
+  end
+
+  def in_journey?
+    @in_use
+  end
+
+  def touch_in
+    raise "Insufficient funds!" if self.balance < MIN_AMOUNT
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use
   end
 end
--- object-oriented-design/oystercard/spec/oystercard_spec.rb
+++ object-oriented-design/oystercard/spec/oystercard_spec.rb
@@ -2,6 +2,9 @@ require "oystercard"
 
 RSpec.describe Oystercard do
   let (:card) { Oystercard.new }
+  let (:limit) { Oystercard::LIMIT }
+  let (:min_amount) { Oystercard::MIN_AMOUNT }
+
   it "has a default value of 0" do
     expect(card.balance).to eq 0
   end
@@ -17,5 +20,27 @@ RSpec.describe Oystercard do
   it "deducts fare" do
     card.top_up(80)
     expect { card.deduct(10) }.to change { card.balance }.to 70
+  end
+
+  it "checks whether card is in use" do
+    card.in_journey?
+    expect(card.in_journey?).to eq false
+  end
+
+  it "checks whether journey has started" do
+    card.top_up(90)
+    card.touch_in
+    expect(card.in_journey?).to be true
+  end
+
+  it "checks whether journey has ended" do
+    card.touch_out
+    expect(card).not_to be_in_journey
+  end
+
+  it "raise error on touch in when balance is lower than £1" do
+    card.top_up(90)
+    card.deduct(90)
+    expect { card.touch_in }.to raise_error "Insufficient funds!"
   end
 end
