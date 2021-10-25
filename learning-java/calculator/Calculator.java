3a933d7a9f23d96c9ac9cf36b32dcc547e5b6f6e
Delia <delia.bute@yahoo.com>
2021-10-25 17:01:17 +0100

    test: used junit and gradle to test drive a calculator app

--- learning-java/calculator/Calculator.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-class Calculator {
-    public static void main(String[] args) {
-        Double result = 2.5 + 2.5;
-        System.out.printf("The result is %.2f \n", result);
-    }
-}
---
+++ learning-java/calculator/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+class Calculator {
+    public static void main(String[] args) {
+        Integer result = 2 + 2;
+        System.out.printf("The result is %d \n", result);
+    }
+
+    public Integer add(int i, int j) {
+        return i + j;
+    }
+
+}
