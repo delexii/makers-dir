e84d42fb96abf561c25e075b169832a1bde349e4
Delia <delia.bute@yahoo.com>
2021-10-29 11:05:21 +0100

    added demo video

--- learning-java/WordGame/app/src/main/java/WordGame/App.java
+++ learning-java/WordGame/app/src/main/java/WordGame/App.java
@@ -22,7 +22,6 @@ public class App {
             } else {
                 System.out.println("You guessed incorrect!");
             }
-            scanner.close();
 
         }
         if (game.isGameLost()) {
